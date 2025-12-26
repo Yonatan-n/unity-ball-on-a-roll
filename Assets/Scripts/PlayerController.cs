3c79a90e58b03f74f2abc215140f0a78c3dd975c
Yonatan Ninio <yonatan.n@proton.me>
2025-12-27 00:55:47 +0200

    camera tracking ball with movment, also xbox controller is working out of the box

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,7 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     float movementX;
     float movementY;
+    [SerializeField] float speed = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
 
     // Update is called once per frame
